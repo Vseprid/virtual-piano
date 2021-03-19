32bbdc9ef1ca1014e8e7b4d979447d61ed2ff95b
Vseprid <vseprid@gmail.com>
2021-03-19 18:18:21 +0200

    feat: task done

--- script.js
+++ script.js
@@ -4,32 +4,11 @@ const piano = document.querySelector('.piano');
 
 const pianoКeys = document.querySelectorAll('.piano-key');
 
+const buttonNote = document.querySelector('.btn-notes');
 
-// piano.addEventListener('mousedown', (event) => {
-// if (event.target.classList.contains('piano-key')) {
-//     pianoКeys.forEach((el) => {
-//         if (el.classList.contains('active')) {
-//             el.classList.remove('active');
-//         }
-//     });
-//     if (event.target.classList.contains('piano-key')) {
-//         event.target.classList.add('active');
-//     }
-// });
+const buttonLetter = document.querySelector(".btn-letters");
 
-
-// piano.addEventListener('mousedown', (event) => {
-//     if (event.target.classList.contains('piano-key')) {
-//         const note = event.target.dataset.note;
-//         const src = `assets/audio/${note}.mp3`;
-//         playAudio(src);
-//     }
-// });
-
-// piano.addEventListener('mouseup', (event) => {
-//     event.target.classList.remove('active')
-// });
-// window.addEventListener('keydown', (event) => playAudio(event));
+const fullscreen = document.querySelector('.fullscreen')
 
 function playAudio(src) {
     const audio = new Audio();
@@ -38,11 +17,8 @@ function playAudio(src) {
     audio.play();
 }
 
-
-
-
 const startSound = (event) => {
-    event.target.classList.add("active");
+    event.target.classList.add("piano-key-active");
     if (event.target.classList.contains('piano-key')) {
         const note = event.target.dataset.note;
         const src = `assets/audio/${note}.mp3`;
@@ -51,17 +27,17 @@ const startSound = (event) => {
 }
 
 const stopSound = (event) => {
-    event.target.classList.remove("active");
-    if (!(event.target.classList.contains("piano-key"))) {
+    event.target.classList.remove("piano-key-active");
+    if (!(event.relatedTarget.classList.contains("piano-key"))) {
         pianoКeys.forEach((elem) => {
-            elem.classList.remove("active");
+            elem.classList.remove("piano-key-active");
         });
     }
 }
 
 const startCorrespondOver = (event) => {
     if (event.target.classList.contains("piano-key")) {
-        event.target.classList.add("active");
+        event.target.classList.add("piano-key-active");
         const note = event.target.dataset.note;
         const src = `assets/audio/${note}.mp3`;
         playAudio(src);
@@ -75,7 +51,7 @@ const startCorrespondOver = (event) => {
 
 const stopCorrespondOver = () => {
     pianoКeys.forEach((elem) => {
-        elem.classList.remove("active");
+        elem.classList.remove("piano-key-active");
         elem.removeEventListener("mouseover", startSound)
         elem.removeEventListener("mouseout", stopSound)
     });
@@ -83,17 +59,57 @@ const stopCorrespondOver = () => {
 }
 
 piano.addEventListener("mousedown", startCorrespondOver, false);
-piano.addEventListener("mouseup", stopCorrespondOver)
+window.addEventListener("mouseup", stopCorrespondOver)
 
-window.addEventListener('keydown', (event) => {
+const startKeboardEvent = (event) => {
     if (event.repeat) return false;
     const letter = event.code[event.code.length - 1];
     pianoКeys.forEach((elem) => {
         if (elem.dataset.letter == letter) {
+            elem.classList.add("piano-key-active");
             const note = elem.dataset.note;
             const src = `assets/audio/${note}.mp3`;
             playAudio(src);
-            // elem.dataset.letter.classList.add("active")
         }
     })
+}
+
+const stopKeyboardEvent = () => {
+    pianoКeys.forEach((elem) => {
+        elem.classList.remove("piano-key-active");
+    });
+}
+
+window.addEventListener('keydown', startKeboardEvent);
+window.addEventListener("keyup", stopCorrespondOver);
+
+
+buttonNote.addEventListener('click', (event) => {
+    if (!event.target.classList.contains('btn-active')) {
+        buttonLetter.classList.remove("btn-active");
+        event.target.classList.add("btn-active");
+        pianoКeys.forEach((elem) => {
+            elem.classList.remove('piano-key-letter')
+        })
+    }
 });
+
+buttonLetter.addEventListener('click', (event) => {
+    if (!event.target.classList.contains('btn-active')) {
+        buttonNote.classList.remove("btn-active");
+        event.target.classList.add("btn-active");
+        pianoКeys.forEach((elem) => {
+            elem.classList.add('piano-key-letter')
+        })
+    }
+});
+
+fullscreen.addEventListener("click", () => {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        }
+    }
+}, false);
